b843effac9c159462fdcddbb492d3dbad9743e19
Prakash2507 <prakash7112006@gmail.com>
2024-10-07 03:59:45 +0000

    added menu

--- src/headers/questionset.h
+++ src/headers/questionset.h
@@ -1,15 +1,21 @@
-
 #ifndef QUESTIONSET_H
 #define QUESTIONSET_H
 
-struct QuestionSet
-{
+enum Category {
+    MATH,
+    GEOGRAPHY,
+    HISTORY,
+    TOTAL_CATEGORIES
+};
+
+struct QuestionSet {
+    int questionNo;
     char question[256];
     char options[4][100];
     int correctOptionIndex;
+    enum Category category; 
 };
 
-
-extern struct QuestionSet quiz_questions[5];
+extern struct QuestionSet quiz_questions[30];
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,59 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "headers/questionset.h"
 
-int main(int argc, char **argv)
-{
+void take_quiz(int category);
+void view_scores();
 
-    const int TOTAL_QUESTIONS = 5;
-    const int TOTAL_OPTIONS = 4;
+  const char *categories[] = {
+        "Math",
+        "Geography",
+        "History"
+    };
+
+int main() {
+
+  
+
+    int choice;
+
+    while (1) {
+        printf("\n<<<< Welcome To Quizzer >>>>\n");
+        printf("1. Take Quiz\n");
+        printf("2. View Scores\n");
+        printf("3. Exit\n");
+        printf("Choose an option: ");
+        
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input! Please enter a number.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+        
+        switch (choice) {
+            case 1:
+                printf("\nChoose Quiz Category:\n");
+
+                for (int c = 0; c < 3; c++) {
+                    printf("%d. %s\n", c+1, categories[c]);
+                }
+
+                printf("Choose an option: ");
+                
+                int category;
+                if (scanf("%d", &category) != 1 || category < 1 || category > 3) {
+                    printf("Invalid category! Please choose between 1 and 3.\n");
+                    continue;
+                }
+                take_quiz(category - 1);
+                break;
+            case 2:
+                view_scores();
+                break;
+            case 3:
+                printf("Exiting the quiz. Goodbye!\n");
+                exit(0);
+            default:
+                printf("Invalid option! Please choose 1, 2, or 3.\n");
+        }
+    }
+
+    return 0;
+}
+
+void take_quiz(int category) {
+    const int TOTAL_QUESTIONS = 5; 
     int score = 0;
     char name[50];
-
-    printf("\n<<<< Welcome To Quizzer >>>>\n\n");
-
-    printf("<<< Enter You Name: ");
+    
+    printf("\n<<< Enter Your Name: ");
     scanf("%49s", name);
     printf("\n");
 
-    for (int i = 0; i < TOTAL_QUESTIONS; i++)
-    {
-        printf("Q%d >>> %s\n", i + 1, quiz_questions[i].question);
-        for (int j = 0; j < TOTAL_OPTIONS; j++)
-        {
-            printf("[%d] > %s\n", j, quiz_questions[i].options[j]);
+    for (int i = 0; i < sizeof(quiz_questions) / sizeof(quiz_questions[0]); i++) {
+        if (quiz_questions[i].category == category) {
+            printf("Q%d >>> %s\n", quiz_questions[i].questionNo, quiz_questions[i].question);
+            for (int j = 0; j < 4; j++) {
+                printf("[%d] > %s\n", j, quiz_questions[i].options[j]);
+            }
+
+            int chosen = -1;
+            while (1) {
+                printf("Choose Option (0-3): ");
+                if (scanf("%d", &chosen) != 1) {
+                    printf("Invalid input! Please enter a number.\n");
+                    while (getchar() != '\n'); // Clear input buffer
+                    continue;
+                }
+                if (chosen >= 0 && chosen < 4) {
+                    break;
+                } else {
+                    printf("Invalid option! Please choose between 0 and 3.\n");
+                }
+            }
+
+            if (chosen == quiz_questions[i].correctOptionIndex) {
+                score += 1;
+            }
+            printf("\n");
         }
-
-        int chosen = -1;
-
-        while (1)
-        {
-            printf("Choose Option (0-%d): ", TOTAL_OPTIONS - 1);
-            if (scanf("%d", &chosen) != 1)
-            {
-                printf("Invalid input! Please enter a number.\n");
-                while (getchar() != '\n')
-                    ;
-                continue;
-            }
-            if (chosen >= 0 && chosen < TOTAL_OPTIONS)
-            {
-                break;
-            }
-            else
-            {
-                printf("Invalid option! Please choose between 0 and %d.\n", TOTAL_OPTIONS - 1);
-            }
-        }
-
-        if (chosen == quiz_questions[i].correctOptionIndex)
-        {
-            score += 1;
-        };
-
-        printf("\n");
-    };
+    }
 
     printf("Quiz Completed, %s! Your Score Is %d/%d\n", name, score, TOTAL_QUESTIONS);
+    FILE *file = fopen("scores.txt", "a");
+    if (file) {
+        fprintf(file, "%s: %d/%d (%s)\n", name, score, TOTAL_QUESTIONS, categories[category]);
+        fclose(file);
+    } else {
+        printf("Error saving score.\n");
+    }
+}
 
-    return 0;
+void view_scores() {
+    FILE *file = fopen("scores.txt", "r");
+    if (file) {
+        char line[256];
+        printf("\n<<< Scores >>>\n");
+        while (fgets(line, sizeof(line), file)) {
+            printf("%s", line);
+        }
+        fclose(file);
+    } else {
+        printf("No scores available.\n");
+    }
 }
